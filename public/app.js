8190f3449894549f42d713d42cc702c7551a28ce
Dev Selimcan <selimcangursu@asemsoft.com>
2025-03-05 22:02:11 +0300

    home slider alanı tasarlandı

--- public/app.js
+++ public/app.js
@@ -18,3 +18,31 @@ let closeSidebar = document.querySelector(".mobile-sidebar");
   } 
 });
 
+
+// Home Slider 
+let currentIndex = 0;
+let backImageButton = document.getElementById('backImageButton')
+let nextImageButton = document.getElementById('nextImageButton')
+let slides = document.querySelector('.slides')
+let totalSlides =  document.querySelectorAll('.slider').length;
+
+nextImageButton.addEventListener('click',function(e){
+   currentIndex++
+   if(currentIndex >= totalSlides)
+   {
+    currentIndex = 0;
+   }
+   
+   slides.style.transform = `translateX(-${currentIndex*33.33}%)`
+})
+
+backImageButton.addEventListener('click',function(e){
+   currentIndex--
+   if(totalSlides >= currentIndex)
+   {
+    currentIndex = 0;
+   }
+   
+   slides.style.transform = `translateX(${currentIndex*33.33}%)`
+})
+
